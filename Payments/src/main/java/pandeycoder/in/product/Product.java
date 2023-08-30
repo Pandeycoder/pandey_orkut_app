99521407a0423ceeacde91e60a32c9cb289ca036
pandeycoder <pandeycoder8474@gmail.com>
2023-08-30 23:36:59 +0530

    ask other developer

--- Payments/src/main/java/pandeycoder/in/product/Product.java
+++ Payments/src/main/java/pandeycoder/in/product/Product.java
@@ -3,14 +3,13 @@ package pandeycoder.in.product;
 public class Product {
 	public static void main(String[] args) {
 		System.out.println("hello.......");
-<<<<<<< HEAD
+
 String x="pandeycoder......";
-=======
 int x=10;
 int y=30;
 String s1="coder...";
 
->>>>>>> af40ed97055a9a216f6117d8ee64102d664549da
+
 	}
 
 }
